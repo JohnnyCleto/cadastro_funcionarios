a363f3a8cedef93b4edd899272651079515890f7
JohnnyCleto <joaovitorcleto129@gmail.com>
2023-10-30 11:05:48 -0300

    V3.2.[FINAL]

--- app.py
+++ app.py
@@ -1,101 +1,68 @@
-from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
+from flask import Flask, render_template, request, redirect
+
 
 app = Flask(__name__)
 
-# Configurações do MySQL
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'MYSQL_USER'
-app.config['MYSQL_PASSWORD'] = 'MYSQL_PASSWORD'
+app.config['MYSQL_USER'] = 'User'
+app.config['MYSQL_PASSWORD'] = 'Password'
 app.config['MYSQL_DB'] = 'empresa'
 
-
-# Função para estabelecer a conexão com o MySQL
-def conectar_mysql():
-    return mysql.connector.connect(
-        host=app.config['MYSQL_HOST'],
-        user=app.config['MYSQL_USER'],
-        password=app.config['MYSQL_PASSWORD'],
-        database=app.config['MYSQL_DB']
-        )
+mydb = mysql.connector.connect(
+    host=app.config['MYSQL_HOST'],
+    user=app.config['MYSQL_USER'],
+    password=app.config['MYSQL_PASSWORD'],
+    database=app.config['MYSQL_DB'])
 
 
-@app.route('/adicionar_funcionario', methods=['GET', 'POST'])
+@app.route('/funcionario', methods=['GET'])
+def get_funcionario():
+    my_cursor = mydb.cursor()
+
+    query = '''
+        SELECT funcionário.id, funcionário.nome, funcionário.sobrenome, funcionário.data_de_admissão, 
+        funcionário.status_funcionário, setor.nome AS setor, cargo.nome AS cargo
+        FROM funcionário
+        LEFT JOIN setor ON funcionário.setor_id = setor.id
+        LEFT JOIN cargo ON funcionário.cargo_id = cargo.id;
+        '''
+
+    my_cursor.execute(query)
+    funcionarios = my_cursor.fetchall()
+
+    return render_template('index.html', funcionarios=funcionarios)  # Passando funcionarios para o template HTML
+
+
+@app.route('/adicionar_funcionario', methods=['POST'])
 def adicionar_funcionario():
-    if request.method == 'GET':
-        try:
-            conn = conectar_mysql()
-            cursor = conn.cursor()
+    nome = request.form['nome']
+    sobrenome = request.form['sobrenome']
+    cargo = request.form['cargo']
+    setor = request.form['setor']
+    data_de_admissao = request.form['data_de_admissao']
+    status_funcionario = 1 if 'status_funcionario' in request.form else 0  # Verifique se o checkbox está marcado
 
-            conn.commit()
-            cursor.close()
-            conn.close()
-            return "Tabelas Setor e Cargo criadas com sucesso."
-        except mysql.connector.Error as err:
-            # Lide com erros de banco de dados aqui
-            print(f"Erro MySQL: {err}")
-            return "Erro ao criar tabelas Setor e Cargo."
-    elif request.method == 'POST':
-        nome = request.form['nome']
-        sobrenome = request.form['sobrenome']
-        data_de_admissão = request.form.get('data_de_admissão')
-        status_funcionário = request.form.get('status_funcionario')
-        cargo_id = request.form.get('cargo_id')
-        setor_id = request.form.get('setor_id')
+    my_cursor = mydb.cursor()
 
-        try:
-            conn = conectar_mysql()
-            cursor = conn.cursor()
+    setor_data = (setor,)
+    setor_insert_query = 'INSERT INTO setor (nome) VALUES (%s)'
+    my_cursor.execute(setor_insert_query, setor_data)
+    setor_id = my_cursor.lastrowid
 
-            # Use uma transação para garantir a consistência dos dados
-            cursor.execute('START TRANSACTION')
+    cargo_data = (cargo, setor_id)
+    cargo_insert_query = 'INSERT INTO cargo (nome, setor_id) VALUES (%s, %s)'
+    my_cursor.execute(cargo_insert_query, cargo_data)
+    cargo_id = my_cursor.lastrowid
 
-            # Inserir dados na tabela funcionário
-            cursor.execute(
-                'INSERT INTO funcionário (nome, sobrenome, data_de_admissão, status_funcionário, cargo_id, setor_id) VALUES (%s, %s, %s, %s, %s, %s)',
-                (nome, sobrenome, data_de_admissão, status_funcionário, cargo_id, setor_id)
-            )
-            # Buscar o nome do cargo da tabela "cargo" com base no ID
-            if cargo_id is not None:
-                cursor.execute('SELECT nome FROM cargo WHERE id = %s', (cargo_id,))
-                cargo_id = cursor.fetchone()[0]
-            else:
-                cargo_id = None
+    funcionario_data = (nome, sobrenome, data_de_admissao, status_funcionario, cargo_id, setor_id)
+    funcionario_insert_query = 'INSERT INTO funcionário (nome, sobrenome, data_de_admissão, status_funcionário, cargo_id, setor_id) VALUES (%s, %s, %s, %s, %s, %s)'
+    my_cursor.execute(funcionario_insert_query, funcionario_data)
 
-            # Buscar o nome do setor da tabela "setor" com base no ID
-            if setor_id is not None:
-                cursor.execute('SELECT nome FROM setor WHERE id = %s', (setor_id,))
-                setor_id = cursor.fetchone()[0]
-            else:
-                setor_id = None
-            conn.commit()
-            cursor.close()
-            conn.close()
-        except mysql.connector.Error as err:
-            # Lide com erros de banco de dados aqui
-            print(f"Erro MySQL: {err}")
-            return "Erro de banco de dados"
-        return redirect(url_for('index'))
+    mydb.commit()
+    my_cursor.close()
 
-@app.route('/')
-def index():
-    try:
-        conn = conectar_mysql()
-        cursor = conn.cursor()
-        cursor.execute('''
-            SELECT f.id, f.nome, f.sobrenome, f.data_de_admissão, f.status_funcionário, c.nome AS cargo, s.nome AS setor
-            FROM funcionário f
-            LEFT JOIN cargo c ON f.cargo_id = c.id
-            LEFT JOIN setor s ON f.setor_id = s.id
-         ''')
-        funcionarios = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        return render_template('index.html', funcionarios=funcionarios)
-    except mysql.connector.Error as err:
-        # Lide com erros de banco de dados aqui
-        print(f"Erro MySQL: {err}")
-        return "Erro de banco de dados"
+    return redirect('/funcionario')
 
 
 if __name__ == '__main__':
